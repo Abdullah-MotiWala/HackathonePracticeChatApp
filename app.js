42c60f267fd8e1a5bc70404e395b5987d656741e
Abdullah-MotiWala <motiwalaabdullah100@gmail.com>
2021-09-17 19:21:21 +0500

    email shown

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-//Firebase Integeration
-const firebaseConfig = {
-    apiKey: "AIzaSyD5eyZ05Zi3x7P_pKrfW9L4Ghg0F0suSqQ",
-    authDomain: "practice-chatapp-b93f1.firebaseapp.com",
-    projectId: "practice-chatapp-b93f1",
-    storageBucket: "practice-chatapp-b93f1.appspot.com",
-    messagingSenderId: "273905711893",
-    appId: "1:273905711893:web:143eff760ebb4c6de6071c",
-    measurementId: "G-42QVG5WD55"
-  };
-firebase.initializeApp(firebaseConfig);
-
-function googleLogin(){
-let provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithPopup(provider).then(alert("Login Successfully")).catch(res=>alert(res))
+function googleLogin() {
+  let provider = new firebase.auth.GoogleAuthProvider();
+  auth.signInWithPopup(provider).catch(res => alert(res))
 };
 
-const facebookLogin = () =>{
+const facebookLogin = () => {
   let facebookProvider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().signInWithPopup(facebookProvider).then(alert("Login Successfull")).catch(res => alert(res))
+  auth.signInWithPopup(facebookProvider).catch(res => alert(res))
+};
 
+const manualSignUp = () => {
+  let email = document.getElementById("userName").value;
+  let password = document.getElementById("password").value;
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      document.getElementById("userName").value = "";
+      document.getElementById("password").value = "";
+    })
+    .catch(err => alert(err));
 }
-function logOut(){
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    firestore.collection("users").doc(user.uid).set({
+      email:user.email
+    })
+    location.replace("mainPage.html");
+    console.log(user.email)
+  }
+})
+
+
+function logOut() {
 
 }
